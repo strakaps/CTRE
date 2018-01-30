57934c8765c22dc386ef0d40c204d4502d4f482c
Peter Straka <p.straka@unsw.edu.au>
2018-01-30 18:22:35 +0800

    quakes -> seaquakes to avoid naming conflict

--- data-raw/quakes_clean.R
+++ data-raw/quakes_clean.R
@@ -1,10 +1,10 @@
 require(parsedate)
-Vipaka <- read.csv("data-raw/quakes.csv")
+Vipaka <- read.csv("data-raw/seaquakes.csv")
 # looks like in early times, measurements weren't as regular, so omit
 Vipaka <- Vipaka[-(1:27), ]
 parse_iso_8601(Vipaka$time) -> Vipaka$time
 
 TT=as.vector(Vipaka$time)
 JJ=Vipaka$mag
-quakes <- data.frame(times = TT, magnitudes = JJ)
-devtools::use_data(quakes, overwrite = TRUE)
+seaquakes <- data.frame(times = TT, magnitudes = JJ)
+devtools::use_data(seaquakes, overwrite = TRUE)
